70fa53f3998e7e480098fd92d542d77676162f12
sangeeth <sangeethkumar.akkaraju@gmail.com>
2020-03-31 20:56:19 +0530

    fix for images

--- application/views/users/account.php
+++ application/views/users/account.php
@@ -7,7 +7,7 @@
 </br></br>
 </br></br></br></br></br></br></br></br>
 <h1> sugessted Pets for you</h1>
-		    	<!--  <td><button class="button nextimage" onclick="return myFunction()">&#10094;</button></td>
+  <td><button class="button nextimage" onclick="javascript: sav[i++]">&#10094;</button>
 
 	<?php  foreach ($data as $geth_index=>$geth) {  
 	     
@@ -20,50 +20,17 @@
 
 
 	<?php } ?> 
-  <td><button class="button nextimage" onclick="javascript: sav[i--]">&#10095;</button>-->
+  <td><button class="button nextimage" onclick="javascript: sav[i--]">&#10095;</button>
   <div class="showpet">
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-	        <div class="carousel-inner" >
-			
-	<?php  for ($i = 3; $i < 10 ;$i++) { 
-  ?>
-
-<td><img id="<?php echo $i?>" class="zoom w3-circle " src="<?php echo base_url()."assets/images/".trim(str_replace(",", " ", $data[$i]->image_path));?>" alt="" border=3 height=200 width=200 style="padding:0 15px 0 15px;"></td>
-
-	<?php } 
-	?>
+	      
 	
 	
 	
 
-<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-
-		    	  
 
 	
 
 </div>
-<script type="text/javascript">
-// Call carousel manually
-$('#myCarouselCustom').carousel();
-
-// Go to the previous item
-$("#prevBtn").click(function(){
-	$("#myCarouselCustom").carousel("prev");
-});
-// Go to the previous item
-$("#nextBtn").click(function(){
-	$("#myCarouselCustom").carousel("next");
-});
-</script>
 
 	
 </div>
