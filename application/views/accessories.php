d6580170735802fe41065da0fba036e1809d9271
vinaykumar517 <appamvinaykumar@gmail.com>
2020-03-31 16:58:42 +0530

    cssvalidation-2

--- application/views/accessories.php
+++ application/views/accessories.php
@@ -1,26 +1,46 @@
+
 <!DOCTYPE html>
 <?php
 include_once "menuWithLogout.php";
-?><br><br>
+?>
+<br>
+<br>
+<br>
+<br>
 <html lang="en">  
 <head>
-<title>Accessories</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900" 	type="text/css" media="all">
-
-<!-- Stylesheet file -->
-<link href="<?php echo base_url('assets/css/style.css'); ?>" rel='stylesheet' type='text/css' />
+<title>pet food</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->  
+  <link rel="icon" type="image/png" href="<?php echo base_url('assets/images/icons/favicon.ico'); ?>"/>
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css"  href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css'); ?>">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css"  href="<?php echo base_url('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css'); ?>">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css"  href="<?php echo base_url('assets/vendor/animate/animate.css'); ?>">
+<!--===============================================================================================-->  
+  <link rel="stylesheet" type="text/css"  href="<?php echo base_url('assets/vendor/css-hamburgers/hamburgers.min.css'); ?>">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/vendor/select2/select2.min.css'); ?>">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/util.css'); ?>" >
+  <link rel="stylesheet" type="text/css"  href="<?php echo base_url('assets/css/mains.css'); ?>">
+<!--===============================================================================================-->
 </head>
 <body>
+<div class="limiter">
 
-<h1>  </h1>	
-<div class="container">
+    <div class="container-login100">
+      <div class="wrap-login100">
+        
 
-	<h2>Accessories</h2>
-	<form action="" method="POST">
-				
+
+            <form class="login100-form validate-form"  action="" method="post" >
+            <h1 align="center" >ACCESSORIES </h1>
+            <br>
+            <br>		
 			<div class="form-group">
 				<input type="text" name="pet_name"   placeholder="PET NAME" required>
 			</div>
@@ -62,12 +82,34 @@ include_once "menuWithLogout.php";
               </div>
           </div-->
 
-		
-			<div class="send-button">
-				<input type="submit" name="signupSubmit" value="SUBMIT">
+		      
+     <div class="container-login100-form-btn">
+            <button class="login100-form-btn" onclick="change()" id="submit" name="signupSubmit"  value="SUBMIT" type="submit">
+              submit
+            </button>
 			</div>
 		</form>
 	</div>
 </div>
+
+
+<!--===============================================================================================-->  
+  <script src="<?php echo base_url('assets/vendor/jquery/jquery-3.2.1.min.js'); ?>" ></script>
+<!--===============================================================================================-->
+  <script  src="<?php echo base_url('assets/vendor/bootstrap/js/popper.js'); ?>"  ></script>
+  <script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.min.js'); ?>"  ></script>
+<!--===============================================================================================-->
+  <script  src="<?php echo base_url('assets/vendor/select2/select2.min.js'); ?>"  ></script>
+<!--===============================================================================================-->
+  <script  src="<?php echo base_url('assets/vendor/tilt/tilt.jquery.min.js'); ?>"  ></script>
+  <script >
+    $('.js-tilt').tilt({
+      scale: 1.1
+    })
+  </script>
+<!--===============================================================================================-->
+  <script  src="<?php echo base_url('assets/js/main.js'); ?>"  ></script>
+
+
 </body>
 </html>
