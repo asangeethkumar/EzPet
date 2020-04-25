3eb62e477c154f58084b40a04133da5470426223
saikumarmadiraju <madirajusaikumar@gmail.com>
2020-04-25 11:36:44 +0530

    updated

--- application/views/vitamins.php
+++ application/views/vitamins.php
@@ -38,10 +38,16 @@ include_once "menuWithLogout.php";
 						<h1 align="center" >VITAMINS AND SUPPLIMENT</h1>
 						<br>
 						<br>
+						<?php if($this->session->flashdata('msg')): ?>
+						<p><?php echo $this->session->flashdata('msg'); ?></p>
+						<?php else : ?>
+						<p><?php echo $this->session->flashdata('error'); ?></p>
+						<?php endif; ?>
+						<br>
 						
 						
 						<div class="wrap-input100 validate-input" data-validate = "valid name is required">
-								<input type="text" name="first_name"  class="input100" placeholder="FIRST NAME" value="<?php echo !empty($user['first_name'])?$user['first_name']:''; ?>" >
+								<input type="text" name="first_name"  class="input100" placeholder="ENTER YOUR NAME"  title="digits  and special characters are not allowed"  value="<?php echo !empty($user['first_name'])?$user['first_name']:''; ?>" >
 								<?php echo form_error('first_name','<p class="help-block">','</p>'); ?>
 								<span class="focus-input100"></span>
 								<span class="symbol-input100">
@@ -53,7 +59,7 @@ include_once "menuWithLogout.php";
 				
 				
 						<div class="wrap-input100 validate-input" data-validate = "Valid email is required: emailId@abc.com">
-								<input type="email"   class="input100" name="email" placeholder="EMAIL" >
+								<input type="email"   class="input100" name="email"   value="<?php echo !empty($user['email'])?$user['email']:''; ?>"placeholder="EMAIL" >
 								<?php echo form_error('email','<p class="help-block">','</p>'); ?>
 								<span class="focus-input100"></span>
 								<span class="symbol-input100">
@@ -64,7 +70,8 @@ include_once "menuWithLogout.php";
 		
 		
 						<div class="wrap-input100 validate-input" data-validate = "Valid phone is required">
-								<input type="text" name="phone"  class="input100" placeholder="PHONENUMBER" value="<?php echo !empty($user['phone'])?$user['phone']:''; ?>">
+								<input type="text" name="phone"  class="input100" 
+								title="Phone number with 6-9 and remaing 9 digit with 0-9" placeholder="PHONENUMBER" value="<?php echo !empty($user['phone'])?$user['phone']:''; ?>">
 								<?php echo form_error('phone','<p class="help-block">','</p>'); ?>
 						
 								<span class="focus-input100"></span>
@@ -76,7 +83,8 @@ include_once "menuWithLogout.php";
 						
 						<div class="wrap-input100 validate-input" data-validate = "  valid vitamin name  is required">
 								<label for="inputState"> </label>
-								<input type="text" name="vitamin"    class="input100"  placeholder="ENTER VITAMIN AND SUPLLIMENT NAME " value="">
+								​<textarea id="txtArea"   name="vitamin"    class="input100"  placeholder="ENTER VITAMIN NAME "  value="" rows="10" cols="70"></textarea>
+								<!--<input type="text" name="vitamin"    class="input100"  placeholder="ENTER VITAMIN NAME " value="">-->
 								
 								<span class="focus-input100"></span>
 								<span class="symbol-input100">
@@ -84,6 +92,14 @@ include_once "menuWithLogout.php";
 								</span>
 						</div>
 						
+						<div class="wrap-input100 validate-input" data-validate = " valid upload file is required">
+								<label>UPLOAD PRESCRIPTION</label>
+								<input type="file" name="upload" accept="image/*" class="input100"  >
+								<span class="focus-input100"></span>
+								<span class="symbol-input100">
+									
+								</span>
+						</div>
 						
 						
 						<div class="wrap-input100 validate-input" data-validate  = " select valid option is required">
@@ -95,6 +111,18 @@ include_once "menuWithLogout.php";
 									<option>2</option>
 									<option>3</option>
 									<option>4</option>
+								 </select>
+						</div>
+						
+						<div class="wrap-input100 validate-input" data-validate  = " valid option is required">
+								<span class="focus-input100"></span>
+								<label for="inputState">QUANTITY ml</label>
+								<select name="quant" id="inputState" class='input100'  >
+									<option> SELECT </option>
+									<option >15 ml</option>
+									<option>20 ml</option>
+									<option>30 ml</option>
+									<option>40 ml</option>
 								 </select>
 						</div>
 
@@ -113,21 +141,23 @@ include_once "menuWithLogout.php";
 							<label for="inputState"> DELIVERY</label>
 							<select name="delivery"  id="inputState" class='input100'>
 									<option>SELECT </option>
-									<option >delivery for one time </option>
-									<option>delivery every month</option>
-									<option>delivery for every two months</option>
-									<option>delivery for every two months</option>
-									<option>delivery for every three  months</option>
-									<option>delivery for every four  months</option>
+									<option >Delivery for one time </option>
+									<option>Delivery every month</option>
+									<option>Delivery for every two months</option>
+									<option>Delivery for every two months</option>
+									<option>Delivery for every three  months</option>
+									<option>Delivery for every four  months</option>
 						  </select>
 						</div>
 				
 				<div class="wrap-input100 validate-input" data-validate = "  valid address is required">
 								<label for="inputState">ADDRESS </label>
-								<input type="text" name="address"  class="input100" >
+								​<textarea id="txtArea" name="address" class="input100" rows="10" cols="70"></textarea>
+								<!--<input type="text" name="address"  class="input100" >-->
 								<span class="focus-input100"></span>
+								<br>
 								<span class="symbol-input100">
-									
+									<i class="fa fa-address-card-o" aria-hidden="true"></i>
 								</span>
 						</div>
 				
